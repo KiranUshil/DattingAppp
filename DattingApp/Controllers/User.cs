c39099b66281d5bb9a1558df39c7f496a1dd97e9
kiranushil <ushilkiran@gmail.com>
2021-08-25 23:24:39 +0530

    Add project files.

---
+++ DattingApp/Controllers/User.cs
@@ -0,0 +1,51 @@
+﻿using DattingApp.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DattingApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class User : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public User(DataContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// [HttpGet] Returns All Users
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        /// <summary>
+        /// Returns specific users
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+
+        
+
+
+
+
+    }
+}
